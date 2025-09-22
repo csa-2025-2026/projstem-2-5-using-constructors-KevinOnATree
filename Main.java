58befefa71104a5772fb462dc5d4f1e0e8a37411
KevinOnATree <kon100@student.hbuhsd.edu>
2025-09-22 21:46:16 +0000

    blah

--- Main.java
+++ Main.java
@@ -4,6 +4,36 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+
+  Scanner sc = new Scanner(System.in);
+
+  Circle circ1 = new Circle();
+  Circle circ2 = new Circle();
+  Circle circ3 = new Circle();
+
+  circ1.setRadius(10.1);
+  circ2.setRadius(14.5);
+  circ3.setRadius(20.5);
+
+  System.out.println(circ1.toString());
+   System.out.println(circ2.toString());
+    System.out.println(circ3.toString());
+
+    double lenWid;
+    double len;
+     double Wid;
+    System.out.println("Type a number for length and width: ");
+    lenWid = sc.nextDouble();
+    System.out.println("Type a number for length: ");
+    len = sc.nextDouble();
+    System.out.println("Type a number for width: ");
+    Wid = sc.nextDouble();
+
+
+
+
+  
+
+
   }
 }
